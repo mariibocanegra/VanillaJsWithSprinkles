b239b05668a655be3d705e92b9009ccebcad6523
Maria Bocanegra <marii.bocanegra@gmail.com>
2016-05-10 21:16:12 -0700

    creates users again

--- newAuthentication/FirebaseLogIn.js
+++ newAuthentication/FirebaseLogIn.js
@@ -1,3 +1,5 @@
+var myRef = new Firebase("https://beesprout.firebaseio.com");
+
 var email = document.getElementById('mail').value;
 var password = document.getElementById('pwd').value;
 var user = document.getElementById('username').value;
@@ -16,46 +18,44 @@ function alreadyExistsDisplay(){
   document.getElementById("descript").innerHTML="Sign In";
 }
 
-var myRef = new Firebase("https://beesprout.firebaseio.com");
 
 
-var authClient = new FirebaseSimpleLogin(myRef, function(error, user) {});
-authClient.createUser(email, password, function(error, user) {
-  if (error === null) {
-    console.log("User created successfully:", user);
-  } else {
-    console.log("Error creating user:", error);
-  }
-});
 
 var authClient = new FirebaseSimpleLogin(myRef, function(error, user) {});
-authClient.login('password', {
- email: '<email@domain.com>',
- password: '<password>'
-});
-
-
-// we would probably save a profile when we register new users on our site
-// we could also read the profile to see if it's null
-// here we will just simulate this with an isNewUser boolean
-// var isNewUser = true;
-// var myRef = new Firebase("beesprout.firebaseapp.com");
-// var authClient = new FirebaseSimpleLogin(myRef, function(error, user) {
-//   if (error) {}
-//   else if (user) {
-//     if(isNewUser) {
-//       // save new user's profile into Firebase so we can
-//       // list users, use them in security rules, and show profiles
-//       myRef.child('users').child(user.uid).set({
-//         displayName: user.displayName,
-//         provider: user.provider,
-//         provider_id: user.id
-//       });
-//     }
+// authClient.createUser(email, password, function(error, user) {
+//   if (error === null) {
+//     console.log("User created successfully:", user);
+//   } else {
+//     console.log("Error creating user:", error);
 //   }
-//   else {}
 // });
-//
+var createUserAction = function(){
+  var email = document.getElementById('mail').value;
+  var password = document.getElementById('pwd').value;
+  var user = document.getElementById('username').value;
+
+myRef.createUser({
+  email: email,
+  password: password
+
+}, function(error, userData) {
+
+  if(error) {
+    console.log("Error creating user:", error);
+  } else {
+    console.log("Successfully created user account with uid: ", userData.uid);
+  }
+  });
+} //closes createUserAction
+
+
+// var authClient = new FirebaseSimpleLogin(myRef, function(error, user) {});
+// authClient.login('password', {
+//  email: '<email@domain.com>',
+//  password: '<password>'
+// });
+
+
 
 
 
